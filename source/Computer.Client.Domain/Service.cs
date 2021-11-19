c773c984f00df903f4cf72f5a0f27ab464c955a0
David Squire <squiredd@gmail.com>
2021-11-18 23:20:20 -0800

    added hosted service

---
+++ source/Computer.Client.Domain/Service.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace Computer.Client.Domain
+{
+    public class Service : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            Console.WriteLine("Starting service...");
+            var tSource = new TaskCompletionSource();
+            stoppingToken.Register(() => tSource.TrySetCanceled());
+            await tSource.Task;
+        }
+    }
+}
--- source/Computer.Client.Host/Program.cs
+++ source/Computer.Client.Host/Program.cs
@@ -1,7 +1,10 @@
+using Computer.Client.Domain;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddHostedService<Service>();
 
 var app = builder.Build();
 
