710fbbfe6573f64428b2328d2afe2d7b7a23c236
David Squire <squiredd@gmail.com>
2021-11-28 20:35:42 -0800

    added signalR hub

--- source/Computer.Client.Host/Program.cs
+++ source/Computer.Client.Host/Program.cs
@@ -1,10 +1,27 @@
 using Computer.Client.Domain;
+using Computer.Client.Host.Hubs;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddHostedService<Service>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+        {
+            builder
+                .WithOrigins("http://localhost:4200")
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials();
+        });
+});
+builder.Services.AddSignalR();
 
 var app = builder.Build();
 
@@ -25,10 +42,13 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+//this must be AFTER UseRouting, but BEFORE UseAuthorization
+app.UseCors();
+
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
+app.MapHub<Bus>("/bus");
 app.Run();
